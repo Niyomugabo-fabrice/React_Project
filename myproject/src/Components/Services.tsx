75cfa99e7acb8c29a7cc2a9a3e01932303a31816
Niyomugabo-fabrice <nfabrice058@gmail.com>
2026-02-13 18:04:13 +0200

    adding the services component

---
+++ myproject/src/Components/Services.tsx
@@ -0,0 +1,9 @@
+function Services(){
+    return(
+        <div className="services">
+            <h2>This is Our Services</h2>
+            <p>in this site we deal with buying and selling goods or product which is ordered</p>
+            <p>Make order</p>
+        </div>
+    )
+}
